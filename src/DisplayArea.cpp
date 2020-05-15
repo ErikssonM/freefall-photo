21d78be3e6ae1106c1ad49ee8ffd02c1f8c3c23e
Martin Eriksson <60point3@gmail.com>
2020-05-15 17:00:57 +0200

    Rewrote DisplayArea to a custom widget instead of QScrollArea child. (Thank you stack overflow) Still needs some work.

--- src/DisplayArea.cpp
+++ src/DisplayArea.cpp
@@ -1,13 +1,17 @@
 #include "DisplayArea.h"
 
 DisplayArea::DisplayArea(QWidget *parent)
-  : QScrollArea(parent)
 {
-  display = new QLabel("Select an image to View");
-  display->setScaledContents(true);
-  display->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
-  setBackgroundRole(QPalette::Dark);
-  setWidget(display);
+  scale = 1.0;
+}
+
+void DisplayArea::paintEvent (QPaintEvent *e)
+{
+  QPainter p{this};
+  p.translate(rect.center());
+  p.scale(scale, scale);
+  p.translate(delta);
+  p.drawPixmap(rect.topLeft(), pixmap);
 }
 
 void DisplayArea::imageSelected(const QModelIndex &current,
@@ -15,20 +19,55 @@ void DisplayArea::imageSelected(const QModelIndex &current,
 {
   if (current.data().canConvert<ImageItem>()) {
     ImageItem selectedImage = qvariant_cast<ImageItem>(current.data());
-    QPixmap pix = QPixmap::fromImage(selectedImage.getImage());
-    display->setPixmap(pix);
-    display->resize(pix.size());
+    pixmap = QPixmap::fromImage(selectedImage.getImage());
+    rect = pixmap.rect();
+    rect.translate(rect.center());
+    update();
   } else {
     qDebug("Attempting to convert ImageItem, but cannot");
   }
 }
 
-void DisplayArea::zoomIn()
+void DisplayArea::mousePressEvent(QMouseEvent *e)
 {
-
+  ref = e->pos();
+  setMouseTracking(true);
 }
 
-void DisplayArea::zoomOut()
+void DisplayArea::mouseMoveEvent(QMouseEvent *e)
 {
+  delta += (e->pos() - ref) * 1.0/scale;
+  ref = e->pos();
+  update();
+}
 
+void DisplayArea::mouseReleaseEvent(QMouseEvent *e)
+{
+  setMouseTracking(false);
+}
+
+void DisplayArea::wheelEvent(QWheelEvent *e)
+{
+  QPoint numDegrees = e->angleDelta();
+
+  if (!numDegrees.isNull()) {
+    float y = numDegrees.y();
+    if (y < 0) {
+      zoom(1.1);
+    } else if (y > 0) {
+      zoom(0.9);
+    }
+  }
+  e->accept();
+}
+
+void DisplayArea::zoom(float factor)
+{
+  scale *= factor;
+  update();
+}
+
+QSize DisplayArea::sizeHint() const
+{
+  return {400, 400};
 }
--- src/DisplayArea.h
+++ src/DisplayArea.h
@@ -5,25 +5,41 @@
 #include <QScrollArea>
 #include <QLabel>
 #include <QModelIndex>
+#include <QWheelEvent>
 
 #include "ImageItem.h"
 
-class DisplayArea : public QScrollArea
+//https://stackoverflow.com/a/40687145
+
+class DisplayArea : public QWidget
 {
   Q_OBJECT
 
 public:
   DisplayArea(QWidget *parent = nullptr);
 
+  QSize sizeHint() const override;
+
 public slots:
   void imageSelected(const QModelIndex &current,
                      const QModelIndex &previous);
-  void zoomOut();
-  void zoomIn();
+  void zoom(float factor);
+
+protected:
+  void paintEvent(QPaintEvent *e) override;
+  void mousePressEvent(QMouseEvent *e) override;
+  void mouseMoveEvent(QMouseEvent *e) override;
+  void mouseReleaseEvent(QMouseEvent *e) override;
+  void wheelEvent(QWheelEvent *e) override;
 
 private:
-  QLabel *display;
-  ImageItem *image;
+  QPixmap pixmap;
+  //ImageItem *image;
+
+  QRectF rect;
+  QPointF ref;
+  QPointF delta;
+  float scale;
 };
 
 #endif
